a453ab7597a832fcfa8781cc4b515f383f049553
Tuan <diabay1999@gmail.com>
2020-12-31 16:19:46 +0700

    home

--- app/src/main/java/com/example/myapplication/Signin.java
+++ app/src/main/java/com/example/myapplication/Signin.java
@@ -1,14 +1,77 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import Common.Common;
+import Model.User;
 
 public class Signin extends AppCompatActivity {
 
+    EditText edtPhone,edtPassword;
+    Button btnSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        edtPassword = (MaterialEditText)findViewById(R.id.editPassword);
+        edtPhone = (MaterialEditText)findViewById(R.id.editPhone);
+        btnSignIn= findViewById(R.id.btnSignIn);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("user");
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ProgressDialog mDialog =new ProgressDialog(Signin.this);
+                mDialog.setMessage("please waiting...");
+                mDialog.show();
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.child(edtPhone.getText().toString()).exists()) {
+                            mDialog.dismiss();
+                            User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
+                            if (user.getPassword().equals(edtPassword.getText().toString())) {
+                                Toast.makeText(Signin.this, "Sing in successfully!", Toast.LENGTH_SHORT).show();
+                                Intent homeIntent =new Intent(Signin.this, Home.class);
+                                Common.currentUser=user;
+                                startActivity(homeIntent);
+                                finish();
+                            } else {
+                                Toast.makeText(Signin.this, "Sing in failed!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else {
+                            mDialog.dismiss();
+                            Toast.makeText(Signin.this, "User not exist in Database", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+
+                });
+            }
+        });
     }
 }
