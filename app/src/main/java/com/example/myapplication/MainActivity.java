c83d0d60b1ba50e4cb7310fbc7f54f85bc5a0450
ledat1011 <superkutex0@gmail.com>
2021-01-03 23:24:36 +0700

    add interface cart

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,21 +3,26 @@ import android.Manifest;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.PermissionRequest;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
 import com.example.myapplication.SignUp;
 import com.example.myapplication.Signin;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnSingIn,btnSingUp;
+    Button btnSingIn,btnSingUp,btnCart;
     TextView txtSlogan;
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener listener;
@@ -28,15 +33,23 @@ public class MainActivity extends AppCompatActivity {
         addControls();
         addEvents();
 
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.nav_cart);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+               TextView t = findViewById(R.id.txt_user);
+               t.setText(item.toString());
+                return false;
+            }
+        });
+
 //
     }
-
     private void addEvents() {
-
         btnSingIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
             }
         });
 
@@ -55,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(SignIn);
             }
         });
+
     }
 
     private void addControls() {
@@ -62,5 +76,8 @@ public class MainActivity extends AppCompatActivity {
         btnSingUp = findViewById(R.id.btnSignup);
         txtSlogan = findViewById(R.id.txtSlogan);
 
+
     }
+
+
 }
